739790804f49aad1d169c8a91cd12242b9570ffe
Tomasz Plewka <tomasz.plewka66@gmail.com>
2021-02-17 11:40:08 -0700

    added firebase config

---
+++ src/js/FirebaseConfig.js
@@ -0,0 +1,15 @@
+// 
+// Firebase Config
+// 
+const FirebaseConfig = (function() {
+    const config = {
+        apiKey: "AIzaSyAAFlexWs0rrp8rrFRYb0RoUFX_23bg7lk",
+        authDomain: "blakczak666.firebaseapp.com",
+        projectId: "blakczak666",
+        storageBucket: "blakczak666.appspot.com",
+        messagingSenderId: "70308342364",
+        appId: "1:70308342364:web:92408b58c2a8e49dd5d499"
+    }
+    return { config };
+})();
+export default FirebaseConfig;
